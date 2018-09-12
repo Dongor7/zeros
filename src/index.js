5f26a7430e2540530254bd17735a4412f1211ef9
Denis Sidorenko <dongor7@gmail.com>
2018-09-12 15:44:27 +0300

    Init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-module.exports = function getZerosCount(number) {
-  // your implementation
+function getZerosCount(number) {
+    let numbers = [];
+
+    for (let i = number; i > 0; i--){
+        numbers.push(i);
+    }
+
+    return getZeroCount(numbers);
 }
+
+function getDigitCount(allFactorNum, digit){
+
+    let count = 0;
+
+    for(let elem of allFactorNum){
+
+        if(digit !== 10){
+            while(true){
+                if((elem % digit === 0) && (elem % 10 !== 0)) {
+                    count++;
+                    elem /= digit;
+                    continue;
+                }
+                if(elem === 50){
+                    count++;
+                    elem /= digit;
+                }
+                else break;
+            }
+        }
+        else{
+            while(elem % digit === 0){
+                count++;
+                elem /= digit;
+            }
+        }
+    }
+
+    return count;
+}
+
+function getZeroCount(allFactorNum){
+
+    let two  = getDigitCount(allFactorNum, 2);
+    let five = getDigitCount(allFactorNum, 5);
+    let ten  = getDigitCount(allFactorNum, 10);
+
+    return two < five ? two + ten : five + ten;
+}
+
+console.log(getZerosCount(10));
+
+module.exports = getZerosCount;
